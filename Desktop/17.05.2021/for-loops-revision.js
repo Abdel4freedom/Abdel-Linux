4f85f8e7e73333b767e09f092ce6798cc708f074
Abdel4freedom <chamati@live.com>
2021-05-17 09:49:24 +0200

    For loops

---
+++ Desktop/17.05.2021/for-loops-revision.js
@@ -0,0 +1,30 @@
+// "Do this thing x 100 "
+
+//   PART A    PART B  PART C
+for (let i = 0; i < 100; i++) {
+    console.log(i + 1);
+}
+
+console.log(`The loop is finished!`);
+
+// another axample
+for (let i = 10; i > 0; i--) {
+    console.log(`${i} green bottles hanging on the wall...And if one green should accidently fall, there'll be ${i-1} green bottles hanging on the wall---`);
+
+
+    // ----------------------------------------------------
+
+    // index:       0    1    2    3
+
+    let myArray = ["A", "B", "C", "D"];
+    //console.log(myArray[i]);
+}
+
+console.log(`The loop is finished);
+
+
+// Parts of the "for" loop declartion :
+
+// PART A : Initialize a variable to keep track of how many iterations ("Loops")you have done so far.
+// PART B : Condition which says how many times you want the "for" loop to iterate ("run").
+// PART C : Iterator : add 1 to the PART A variable, every time you go through one iteration.
